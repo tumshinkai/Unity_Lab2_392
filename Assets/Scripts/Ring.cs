aee0ceff1b896f539adbe69cebbebca22252f151
TumTummy <141399784+tumshinkai@users.noreply.github.com>
2024-07-18 18:14:08 +0700

    first commit

---
+++ Assets/Scripts/Ring.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+    public GameObject prefab;
+    public int numberOfObjects;
+    public float radius;
+
+    // Start is called before the first frame update
+    void MakeCircle()
+    {
+        // Degrees-to-radians conversion constant (Read Only).
+        // This is equal to (PI * 2) / 360.
+        float angle = 360f / numberOfObjects;
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            float x = Mathf.Sin(Mathf.Deg2Rad * angle * i) * radius;
+            float z = Mathf.Cos(Mathf.Deg2Rad * angle * i) * radius;
+            Vector3 position = new Vector3(x, 0.5f, z);
+            Instantiate(prefab, position, Quaternion.identity);
+        }
+    }
+
+    void Start()
+    {
+        MakeCircle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
